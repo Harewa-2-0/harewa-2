86e7edf6373c11b6fb522562059cccd47c5753b7
Audu <empire4josh@gmail.com>
2025-07-01 00:42:42 +0100

    feat: finish landing page layout and grid responsiveness

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -1,7 +1,18 @@
 'use client';
-
+import TrendingFashionStyles from '@/components/Public_C/Home/trending_Fashion';
 import Hero from '@/components/Public_C/Home/hero';
+import What_we_Offer from '@/components/Public_C/Home/what_We_Offer';
+import ProductCardsGrid from '@/components/Public_C/Home/new_Arivals';
+import FashionCardsComponent from '@/components/Public_C/Home/fashion_Journey';
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <div>
+      <Hero />
+      <What_we_Offer />
+      <TrendingFashionStyles/>
+      <ProductCardsGrid/>
+      <FashionCardsComponent/>
+    </div>
+  );
 }
