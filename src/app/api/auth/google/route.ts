f5283af7610b991867c6ad2a23dd62c2cd380427
samuel ibitoye <ibitoyesamuel25@gmail.com>
2025-06-30 16:43:21 +0100

    Clean up Auth

--- src/app/api/auth/google/route.ts
+++ src/app/api/auth/google/route.ts
@@ -1,12 +1,18 @@
-// /app/api/auth/google/route.ts (or /pages/api/auth/google.ts for pages router)
+import { NextResponse } from "next/server";
 
-import passport from 'passport';
-import '@/lib/passport'; // where your GoogleStrategy is configured
-import { NextApiRequest, NextApiResponse } from 'next';
+export async function GET() {
+    const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
+    const REDIRECT_URI = process.env.GOOGLE_CALLBACK_URL!;
+    console.log("GOOGLE_CLIENT_ID", GOOGLE_CLIENT_ID);
+    console.log("REDIRECT_URI", REDIRECT_URI);
+    const authURL = new URL("https://accounts.google.com/o/oauth2/v2/auth");
+    authURL.searchParams.set("client_id", GOOGLE_CLIENT_ID);
+    authURL.searchParams.set("redirect_uri", REDIRECT_URI);
+    authURL.searchParams.set("response_type", "code");
+    authURL.searchParams.set("scope", "openid email profile");
+    authURL.searchParams.set("prompt", "select_account");
+    authURL.searchParams.set("include_granted_scopes", "true");
+    authURL.searchParams.set("state", "pass-through value");
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    passport.authenticate('google', {
-        scope: ['profile', 'email'],
-        session: false, // important since you're not using sessions
-    })(req, res);
+    return NextResponse.redirect(authURL.toString());
 }
--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   username: { type: String, unique: true },
   phoneNumber: { type: String, unique: true },
   isVerified: { type: Boolean, default: false },
