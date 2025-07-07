07f80397c85daec242f04da19c54b18f2a920b24
Audu <empire4josh@gmail.com>
2025-07-07 17:22:52 +0100

    Remove .env.local and add to .gitignore

--- src/app/(public)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import PublicLayout from "@/components/Public_C/Layout/publiclayout";
-
-export default function PublicPagesLayout({ children }: { children: React.ReactNode }) {
-  return <PublicLayout>{children}</PublicLayout>;
-}
