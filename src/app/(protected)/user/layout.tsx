64470109ca088299690f10759396c21b740c3dfc
Audu <empire4josh@gmail.com>
2025-08-02 14:45:41 +0100

    feat: Scafolded the Files and Layout Structure for the user Profile Page

---
+++ src/app/(protected)/user/layout.tsx
@@ -0,0 +1,5 @@
+import ProfileLayout from '@/components/Public_C/profile/profile-layout';
+
+export default function ProfilePageLayout({ children }: { children: React.ReactNode }) {
+  return <ProfileLayout>{children}</ProfileLayout>;
+}
