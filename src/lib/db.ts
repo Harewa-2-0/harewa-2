1d1ed5c91677397bc61ca9459b5160a6db323a4b
Audu Joshua Adinoyi <empire4josh@gmail.com>
2026-01-14 11:38:06 +0100

    fix: Mongose Connection Error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,15 +6,45 @@ if (!MONGO_URI) {
   throw new Error(" please define mongo environment variable");
 }
 
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 async function connectDB() {
-  if (mongoose.connection.readyState === 1) {
-    return mongoose;
+  if (cached.conn) {
+    return cached.conn;
   }
-  const opts = {
-    bufferCommands: false,
-  };
-  await mongoose.connect(MONGO_URI!, opts);
-  return mongoose;
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGO_URI!, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
 }
 
 export default connectDB;
