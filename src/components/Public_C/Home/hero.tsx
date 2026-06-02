1426eb6d9e2a62e24e190d2e3f2ee86c09fa7c3e
Audu <empire4josh@gmail.com>
2026-06-02 12:24:56 +0100

    test: Testing Commit

--- src/components/Public_C/Home/hero.tsx
+++ src/components/Public_C/Home/hero.tsx
@@ -62,7 +62,7 @@ const HeroSection = () => {
             >
               Where Innovation
               <br />
-              Meets Fashion
+              Meets Fashion (Dev Test)
             </motion.h1>
 
             <motion.p
